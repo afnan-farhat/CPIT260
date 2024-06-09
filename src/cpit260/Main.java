56f6df5cd9c0350bbe5c7ad05f791374f34af819
Afnan Farhat <4fnan.f@gmail.com>
2024-06-09 11:17:31 +0300

    after refactoring

---
+++ src/cpit260/Main.java
@@ -0,0 +1,10 @@
+package cpit260;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MemoryManager memoryManager = new MemoryManager();
+        memoryManager.run();
+        
+    }
+}
